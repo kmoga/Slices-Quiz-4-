0af369f7ffaac38c60e37b11274427e63abb0727
SBAUser <sbauser@sbausers-macbook-pro.local>
2019-09-19 16:50:13 -0700

    quiz 4

--- Slices (Quiz 4)/ViewController.swift
+++ Slices (Quiz 4)/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slices: UITextField!
+    @IBOutlet weak var mouths: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func calculate(_ sender: UIButton) {
+//        if let slices = Double(slices.text!) {
+//            if let mouths = Double(mouths.text!) {
+//                if mouths != 0 {
+//                    resultLabel.text = "Everyone receives \(slices/mouths) slices."
+//                } else {
+//                    resultLabel.text = "Cannot divide pizza by 0."
+//                }
+//            } else {
+//                resultLabel.text = "Please enter a valid number of mouths."
+//            }
+//        } else {
+//            resultLabel.text = "Please enter a valid number of slices."
+//        }
+    guard let slices = Double(slices.text!) else {
+    resultLabel.text = "Please elner a valid number of slices"
+        return
+    }
+        guard let mouths = Double(mouths.text!) else {
+            resultLabel.text = "Please enter a valid number of mouths"
+            return
+        }
+        guard mouths != 0 else {
+            resultLabel.text = "Cannot have 0 mouths. Its impossible!."
+            return
+        }
+        guard slices != 0 else {
+            resultLabel.text = "There is no pizza left. Should've gotten there sooner."
+            return
+        }
+        resultLabel.text = "Everybody gets \(slices/mouths) slices."
 }
 
+}
